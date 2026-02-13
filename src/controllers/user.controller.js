b5accb41b34a96878d9c9419c4d48f2dcdd64a78
Ashu12tosh <ash8707672263@gmail.com>
2026-02-13 18:23:07 +0530

    import bug fix jwt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import jwr from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefeshTokens = async (userId) => {
   try {
